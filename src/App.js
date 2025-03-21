b1f60f182c161f74e4427371b4fb0215513932e7
Kushagra Shandilya <22053255@kiit.ac.in>
2025-03-22 03:12:01 +0530

    add statistics.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import AuthPage from "./AuthPage";
 import LandingPage from "./LandingPage";
+import Statistics from "./Statistics";
 
 function App() {
   return (
@@ -9,8 +10,8 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/auth" element={<AuthPage />} />
+        <Route path="/statistics" element={<Statistics />} />
       </Routes>
-      
     </Router>
   );
 }
